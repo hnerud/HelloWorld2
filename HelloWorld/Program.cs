af8ed5ab8ca9032b9ec669cf17a333d9b14d436a
hnerud <hnerud@applesauseinteractive.com>
2015-07-14 17:14:40 -0500

    changed the text

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.WriteLine("Let's make some games.");
+            System.Console.ReadKey();
+        }
+    }
+}
